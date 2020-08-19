680e434405555c118c718f9071756a406359ae83
Niko4u5 <niko4u@fam-email.de>
2020-08-19 13:14:28 +0200

    Folowd Danis Tutorial
    https://www.youtube.com/watch?v=Xgh4v1w5DxU

---
+++ GrappleGame/Assets/MouseLook.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        playerBody.Rotate(Vector3.up * mouseX);
+        playerBody.Rotate(Vector3.left * mouseY);
+    }
+}
