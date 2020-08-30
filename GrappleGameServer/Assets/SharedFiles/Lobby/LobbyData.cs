a6c8ef208e8ecfc5c72bb97d6ade8bf40f7a23ba
maart <maart@DESKTOP-R5T2CB9>
2020-08-30 20:29:28 +0100

    Lobbysytem (runs till Lobby)

---
+++ GrappleGameServer/Assets/SharedFiles/Lobby/LobbyData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace SharedFiles.Lobby
+{
+    public class LobbyData : MonoBehaviour
+    {
+        [SerializeField] public GameObject[] snappingObjects;
+        public static LobbyData instance;
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Debug.Log("Instance already exists, destroying object!");
+                Destroy(this);
+            }
+
+            // proper ids
+            for (int i = 0; i < snappingObjects.Length; i++)
+            {
+                snappingObjects[i].name = "snap " + i;
+            }
+        }
+    }
+}
